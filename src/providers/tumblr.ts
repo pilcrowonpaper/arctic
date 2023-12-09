a593387648dba0718148cc1575575aa4c58ade4a
mikah13 <anhminhhoang13@gmail.com>
2023-12-08 16:39:40 -0800

    update: added functions to Tumblr class

--- src/providers/tumblr.ts
+++ src/providers/tumblr.ts
@@ -1,6 +1,7 @@
 import { OAuth2Client } from "oslo/oauth2";
 
 import type { OAuth2Provider } from "../index.js";
+import { TimeSpan, createDate } from "oslo";
 
 const authorizeEndpoint = "https://www.tumblr.com/oauth2/authorize";
 const tokenEndpoint = "https://api.tumblr.com/v2/oauth2/token";
@@ -27,5 +28,73 @@ export class Tumblr implements OAuth2Provider {
 		this.scope.push("basic");
 	}
 
-	
+	public async createAuthorizationURL(state: string): Promise<URL> {
+		return await this.client.createAuthorizationURL({
+			state,
+			scope: this.scope
+		});
+	}
+
+	public async validateAuthorizationCode(code: string): Promise<TumblrTokens> {
+		const result = await this.client.validateAuthorizationCode<TokenResponseBody>(code, {
+			credentials: this.clientSecret
+		});
+
+		return {
+			accessToken: result.access_token,
+			refreshToken: result.refresh_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s"))
+		};
+	}
+
+	public async getUser(accessToken: string): Promise<TumblrUser> {
+		const response = await fetch(userinfoEndpoint, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+		return await response.json();
+	}
+
+	public async refreshAccessToken(refreshToken: string): Promise<TumblrTokens> {
+		const result = await this.client.refreshAccessToken<TokenResponseBody>(refreshToken, {
+			authenticateWith: "request_body",
+			credentials: this.clientSecret
+		});
+
+		return {
+			accessToken: result.access_token,
+			refreshToken: result.refresh_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s"))
+		};
+	}
+}
+
+interface TokenResponseBody {
+	access_token: string;
+	expires_in: number;
+	refresh_token: string;
+}
+export interface TumblrTokens {
+	accessToken: string;
+	accessTokenExpiresAt: Date;
+	refreshToken: string;
+}
+
+export interface TumblrUser {
+	following: number;
+	default_post_format: "html" | "markdown" | "raw";
+	name: string;
+	likes: number;
+	blogs: TumblrBlog[];
+}
+
+export interface TumblrBlog {
+	name: string;
+	url: string;
+	title: string;
+	primary: boolean;
+	followers: number;
+	tweet: "auto" | "Y" | "N";
+	type: "public" | "private";
 }
