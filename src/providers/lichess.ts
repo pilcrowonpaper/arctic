fd30b9ce463aa67cf86d2ca0092dae9ad19f7f57
sukalov <matvey10@gmail.com>
2024-01-22 04:58:03 +0300

    fix: rm lichess clientSecret, refactor

--- src/providers/lichess.ts
+++ src/providers/lichess.ts
@@ -6,66 +6,52 @@ const tokenEndpoint = "https://lichess.org/api/token";
 
 export class Lichess implements OAuth2ProviderWithPKCE {
 	private client: OAuth2Client;
-	private clientSecret: string;
 	private clientId: string;
-	private options: {
-		redirectURI: string;
-		scope: string[];
+    private redirectURI: string;
+	private options?: {
+		scopes?: string[];
 	};
 
 	constructor(
 		clientId: string,
-		clientSecret: string,
-		options: {
-			redirectURI: string;
-			scope: string[];
+        redirectURI: string,
+		options?: {
+			scopes?: string[]
 		}
 	) {
 		this.client = new OAuth2Client(clientId, authorizeEndpoint, tokenEndpoint, {
-			redirectURI: options?.redirectURI
+			redirectURI
 		});
-		this.clientSecret = clientSecret;
-		this.options = options;
 		this.clientId = clientId;
+        this.redirectURI = redirectURI;
+		this.options = options ?? {};
 	}
 
 	public async createAuthorizationURL(
 		state: string,
 		codeVerifier: string,
 		options?: {
-			scope: string[];
+			scopes?: string[];
 		}
 	): Promise<URL> {
 		return await this.client.createAuthorizationURL({
 			state,
-			scopes: options?.scope ?? [],
+			scopes: options?.scopes ?? [],
 			codeVerifier
 		});
 	}
 
 	public async validateAuthorizationCode(code: string, codeVerifier: string): Promise<LichessTokens> {
-		const options: ValidateAuthorizationCodeRequestOptions = {
-			authenticateWith: "request_body",
-			clientId: this.clientId,
-			codeVerifier,
-			redirectURI: this.options.redirectURI,
-			credentials: this.clientSecret
-		};
-
-		const result = await this.client.validateAuthorizationCode(code, options);
+		const result = await this.client.validateAuthorizationCode(code, 
+            {
+                authenticateWith: "request_body",
+                codeVerifier,
+            });
 		const tokens: LichessTokens = {
 			accessToken: result.access_token
 		};
 		return tokens;
 	}
-}
-
-interface ValidateAuthorizationCodeRequestOptions {
-    authenticateWith: "request_body" | undefined,
-    clientId: string,
-    codeVerifier: string,
-    redirectURI: string,
-    credentials: string
 }
 
 export interface LichessTokens {
