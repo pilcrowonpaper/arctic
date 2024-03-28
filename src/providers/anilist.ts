423a9e8426c8106c3376926ae16fbd3833acff18
sayykii <sayykiipersonal@gmail.com>
2024-03-28 23:51:32 +0200

    feat: implement anilist provider

---
+++ src/providers/anilist.ts
@@ -0,0 +1,39 @@
+import { OAuth2Client } from "oslo/oauth2";
+import type { OAuth2Provider } from "../index.js";
+
+const authorizeEndpoint = "https://anilist.co/api/v2/oauth/authorize";
+const tokenEndpoint = "https://anilist.co/api/v2/oauth/token";
+
+export class Anilist implements OAuth2Provider {
+  private client: OAuth2Client;
+  private clientSecret: string;
+
+  constructor(clientId: string, clientSecret: string, redirectURI: string) {
+    this.client = new OAuth2Client(clientId, authorizeEndpoint, tokenEndpoint, {
+      redirectURI
+    });
+    this.clientSecret = clientSecret;
+  }
+
+  public async createAuthorizationURL(): Promise<URL> {
+    return await this.client.createAuthorizationURL();
+  }
+
+    public async validateAuthorizationCode(code: string): Promise<AnilistTokens> {
+        const result = await this.client.validateAuthorizationCode<TokenResponseBody>(code, {
+            credentials: this.clientSecret
+        });
+        const tokens: AnilistTokens = {
+            accessToken: result.access_token
+        };
+        return tokens;
+    }
+}
+
+interface TokenResponseBody {
+    access_token: string;
+}
+
+export interface AnilistTokens {
+    accessToken: string;
+}
