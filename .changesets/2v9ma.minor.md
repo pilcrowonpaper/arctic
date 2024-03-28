5d83142c59f217a046df6b86802f1c82ded0f494
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-03-28 15:06:19 +0900

    add myanimelist provider

---
+++ .changesets/2v9ma.minor.md
@@ -0,0 +1,1 @@
+Add MyAnimeList provider.
---
+++ src/providers/myanimelist.ts
@@ -0,0 +1,77 @@
+import { TimeSpan, createDate } from "oslo";
+import { OAuth2Client } from "oslo/oauth2";
+
+import type { OAuth2ProviderWithPKCE } from "../index.js";
+
+const authorizeEndpoint = "https://myanimelist.net/v1/oauth2/authorize";
+const tokenEndpoint = "https://myanimelist.net/v1/oauth2/token";
+
+export class MyAnimeList implements OAuth2ProviderWithPKCE {
+	private client: OAuth2Client;
+	private clientSecret: string;
+
+	constructor(
+		clientId: string,
+		clientSecret: string,
+		options?: {
+			redirectURI?: string;
+		}
+	) {
+		this.client = new OAuth2Client(clientId, authorizeEndpoint, tokenEndpoint, {
+			redirectURI: options?.redirectURI
+		});
+		this.clientSecret = clientSecret;
+	}
+
+	public async createAuthorizationURL(state: string, codeVerifier: string): Promise<URL> {
+		return await this.client.createAuthorizationURL({
+			state,
+			codeVerifier,
+			codeChallengeMethod: "plain"
+		});
+	}
+
+	public async validateAuthorizationCode(
+		code: string,
+		codeVerifier: string
+	): Promise<MyAnimeListTokens> {
+		const result = await this.client.validateAuthorizationCode<TokenResponseBody>(code, {
+			credentials: this.clientSecret,
+			codeVerifier
+		});
+		const tokens: MyAnimeListTokens = {
+			accessToken: result.access_token,
+			refreshToken: result.refresh_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s")),
+			idToken: result.id_token
+		};
+		return tokens;
+	}
+
+	public async refreshAccessToken(refreshToken: string): Promise<MyAnimeListTokens> {
+		const result = await this.client.refreshAccessToken<TokenResponseBody>(refreshToken, {
+			credentials: this.clientSecret
+		});
+		const tokens: MyAnimeListTokens = {
+			accessToken: result.access_token,
+			refreshToken: result.refresh_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s")),
+			idToken: result.id_token
+		};
+		return tokens;
+	}
+}
+
+interface TokenResponseBody {
+	access_token: string;
+	refresh_token: string;
+	expires_in: number;
+	id_token: string;
+}
+
+export interface MyAnimeListTokens {
+	accessToken: string;
+	refreshToken: string;
+	accessTokenExpiresAt: Date;
+	idToken: string;
+}
