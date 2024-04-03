6bb8e23540c6de0c5adc4120dc77080e85a64ee3
epotter2297 <65096111+epotter2297@users.noreply.github.com>
2024-04-03 09:27:45 -0400

    Add Intuit provider (#97)
    * feat: implement intuit provider
    
    * docs: add docs for intuit provider
    
    * chore: add id token
    
    * chore: fix provider authentication and update readme
    
    ---------
    
    Co-authored-by: Elijah Potter <epotter@smoothlogics.com>

---
+++ src/providers/intuit.ts
@@ -0,0 +1,77 @@
+import { OAuth2Client } from "oslo/oauth2";
+import { TimeSpan, createDate } from "oslo";
+
+import type { OAuth2Provider } from "../index.js";
+
+const authorizeEndpoint = "https://appcenter.intuit.com/connect/oauth2";
+const tokenEndpoint = "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer";
+
+export class Intuit implements OAuth2Provider {
+	private client: OAuth2Client;
+	private clientSecret: string;
+
+	constructor(clientId: string, clientSecret: string, redirectURI: string) {
+		this.client = new OAuth2Client(clientId, authorizeEndpoint, tokenEndpoint, {
+			redirectURI
+		});
+		this.clientSecret = clientSecret;
+	}
+
+	public async createAuthorizationURL(
+		state: string,
+		options?: {
+			scopes?: string[];
+		}
+	): Promise<URL> {
+		const scopes = options?.scopes ?? [];
+		return await this.client.createAuthorizationURL({
+			state,
+			scopes: [...scopes, "openid"]
+		});
+	}
+
+	public async validateAuthorizationCode(code: string): Promise<IntuitTokens> {
+		const result = await this.client.validateAuthorizationCode<TokenResponseBody>(code, {
+			credentials: this.clientSecret
+		});
+		const tokens: IntuitTokens = {
+			accessToken: result.access_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s")),
+			refreshToken: result.refresh_token,
+			refreshTokenExpiresAt: createDate(new TimeSpan(result.x_refresh_token_expires_in, "s")),
+			idToken: result.id_token
+		};
+		return tokens;
+	}
+
+	public async refreshAccessToken(accessToken: string): Promise<IntuitTokens> {
+		const result = await this.client.refreshAccessToken<TokenResponseBody>(accessToken, {
+			credentials: this.clientSecret
+		});
+		const tokens: IntuitTokens = {
+			accessToken: result.access_token,
+			accessTokenExpiresAt: createDate(new TimeSpan(result.expires_in, "s")),
+			refreshToken: result.refresh_token,
+			refreshTokenExpiresAt: createDate(new TimeSpan(result.x_refresh_token_expires_in, "s")),
+			idToken: result.id_token
+		};
+		return tokens;
+	}
+}
+
+interface TokenResponseBody {
+	token_type: "bearer";
+	expires_in: number;
+	refresh_token: string;
+	x_refresh_token_expires_in: number;
+	access_token: string;
+	id_token: string;
+}
+
+export interface IntuitTokens {
+	accessToken: string;
+	accessTokenExpiresAt: Date;
+	refreshToken: string;
+	refreshTokenExpiresAt: Date;
+	idToken: string;
+}
