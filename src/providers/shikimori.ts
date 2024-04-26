0274fe42fbcd8ad8c93be2e9aa6a58e14f46fcc1
neverlane <naer200469@gmail.com>
2024-04-26 09:54:27 +0300

    ShikimoriTokens -> TokenResponseBody and remove export, ShikimoriTokens interface for validateAuthorizationCode

--- src/providers/shikimori.ts
+++ src/providers/shikimori.ts
@@ -4,15 +4,6 @@ import type { OAuth2Provider, Tokens } from "../index.js";
 const authorizeEndpoint = "https://shikimori.one/oauth/authorize";
 const tokenEndpoint = "https://shikimori.one/oauth/token";
 
-export interface ShikimoriTokens {
-  access_token: string;
-  token_type: string;
-  expires_in: number;
-  refresh_token: string;
-  scope: string;
-  created_at: number;  
-}
-
 export class Shikimori implements OAuth2Provider {
   private client: OAuth2Client;
   private clientSecret: string;
@@ -24,12 +15,12 @@ export class Shikimori implements OAuth2Provider {
     this.clientSecret = clientSecret;
   }
 
-  createAuthorizationURL(state: string): Promise<URL> {
-    return this.client.createAuthorizationURL({ state });
+  createAuthorizationURL(state: string, scopes?: string[]): Promise<URL> {
+    return this.client.createAuthorizationURL({ state, scopes });
   }
   
-  async validateAuthorizationCode(code: string): Promise<Tokens> {
-    const result = await this.client.validateAuthorizationCode<ShikimoriTokens>(code, {
+  async validateAuthorizationCode(code: string): Promise<ShikimoriTokens> {
+    const result = await this.client.validateAuthorizationCode<TokenResponseBody>(code, {
       authenticateWith: "request_body",
       credentials: this.clientSecret
     });
@@ -42,7 +33,7 @@ export class Shikimori implements OAuth2Provider {
   }
 
   async refreshAccessToken(refreshToken: string): Promise<Tokens> {
-    const result = await this.client.refreshAccessToken<ShikimoriTokens>(refreshToken, {
+    const result = await this.client.refreshAccessToken<TokenResponseBody>(refreshToken, {
       authenticateWith: "request_body",
       credentials: this.clientSecret
     });
@@ -53,4 +44,19 @@ export class Shikimori implements OAuth2Provider {
       accessTokenExpiresAt: new Date((result.created_at + result.expires_in) * 1000),
     };
   }
+}
+
+interface TokenResponseBody {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  created_at: number;  
+}
+
+export interface ShikimoriTokens {
+	accessToken: string;
+	refreshToken: string;
+	accessTokenExpiresAt: Date;
 }
