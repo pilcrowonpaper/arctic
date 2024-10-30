d6b91a46af146e916d2be2ef9cdc24812fed2f52
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-30 09:48:52 +0900

    format

--- src/providers/naver.ts
+++ src/providers/naver.ts
@@ -18,42 +18,34 @@ export class Naver {
 
 	public createAuthorizationURL(state: string): URL {
 		const url = new URL(authorizationEndpoint);
-
 		url.searchParams.set("response_type", "code");
 		url.searchParams.set("client_id", this.clientId);
 		url.searchParams.set("redirect_uri", this.redirectURI);
 		url.searchParams.set("state", state);
-
 		return url;
 	}
 
 	public async validateAuthorizationCode(state: string, code: string): Promise<OAuth2Tokens> {
 		const body = new URLSearchParams();
-
 		body.set("grant_type", "authorization_code");
 		body.set("client_id", this.clientId);
 		body.set("client_secret", this.clientSecret);
 		body.set("code", code);
 		body.set("state", state);
 		body.set("redirect_uri", this.redirectURI);
-
 		const request = createOAuth2Request(tokenEndpoint, body);
 		const tokens = await sendTokenRequest(request);
-
 		return tokens;
 	}
 
 	public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
 		const body = new URLSearchParams();
-
 		body.set("grant_type", "refresh_token");
 		body.set("client_id", this.clientId);
 		body.set("client_secret", this.clientSecret);
 		body.set("refresh_token", refreshToken);
-
 		const request = createOAuth2Request(tokenEndpoint, body);
 		const tokens = await sendTokenRequest(request);
-
 		return tokens;
 	}
 }
