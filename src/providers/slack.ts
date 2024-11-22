4e79b5245b242486542ed4a81300579ed73e0daf
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-11-22 15:30:08 +0900

    update slack provider

--- src/providers/slack.ts
+++ src/providers/slack.ts
@@ -1,4 +1,4 @@
-import { createOAuth2Request, encodeBasicCredentials, sendTokenRequest } from "../request.js";
+import { OAuth2Client } from "../client.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
@@ -6,39 +6,19 @@ const authorizationEndpoint = "https://slack.com/openid/connect/authorize";
 const tokenEndpoint = "https://slack.com/api/openid.connect.token";
 
 export class Slack {
-	private clientId: string;
-	private clientSecret: string;
-	private redirectURI: string | null;
+		private client: OAuth2Client;
 
 	constructor(clientId: string, clientSecret: string, redirectURI: string | null) {
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.redirectURI = redirectURI;
+		this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
 	}
 
 	public createAuthorizationURL(state: string, scopes: string[]): URL {
-		const url = new URL(authorizationEndpoint);
-		url.searchParams.set("response_type", "code");
-		url.searchParams.set("client_id", this.clientId);
-		url.searchParams.set("state", state);
-		url.searchParams.set("scope", scopes.join(" "));
-		if (this.redirectURI !== null) {
-			url.searchParams.set("redirect_uri", this.redirectURI);
-		}
+		const url = this.client.createAuthorizationURL(authorizationEndpoint, state, scopes);
 		return url;
 	}
 
 	public async validateAuthorizationCode(code: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "authorization_code");
-		body.set("code", code);
-		if (this.redirectURI !== null) {
-			body.set("redirect_uri", this.redirectURI);
-		}
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.validateAuthorizationCode(tokenEndpoint, code, null);
 		return tokens;
 	}
 }
