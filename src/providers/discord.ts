2c53401913a6ef3304ea4a366c53c9413d9b39f6
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-11-22 15:10:57 +0900

    add discord provider

--- src/providers/discord.ts
+++ src/providers/discord.ts
@@ -1,9 +1,4 @@
-import {
-	createOAuth2Request,
-	encodeBasicCredentials,
-	sendTokenRequest,
-	sendTokenRevocationRequest
-} from "../request.js";
+import { OAuth2Client } from "../client.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
@@ -12,55 +7,28 @@ const tokenEndpoint = "https://discord.com/api/oauth2/token";
 const tokenRevocationEndpoint = "https://discord.com/api/oauth2/token/revoke";
 
 export class Discord {
-	private clientId: string;
-	private clientSecret: string;
-	private redirectURI: string;
+	private client: OAuth2Client;
 
 	constructor(clientId: string, clientSecret: string, redirectURI: string) {
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.redirectURI = redirectURI;
+		this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
 	}
 
 	public createAuthorizationURL(state: string, scopes: string[]): URL {
-		const url = new URL(authorizationEndpoint);
-		url.searchParams.set("response_type", "code");
-		url.searchParams.set("client_id", this.clientId);
-		url.searchParams.set("state", state);
-		url.searchParams.set("scope", scopes.join(" "));
-		url.searchParams.set("redirect_uri", this.redirectURI);
+		const url = this.client.createAuthorizationURL(authorizationEndpoint, state, scopes);
 		return url;
 	}
 
 	public async validateAuthorizationCode(code: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "authorization_code");
-		body.set("code", code);
-		body.set("redirect_uri", this.redirectURI);
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.validateAuthorizationCode(tokenEndpoint, code, null);
 		return tokens;
 	}
 
 	public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "refresh_token");
-		body.set("refresh_token", refreshToken);
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.refreshAccessToken(tokenEndpoint, refreshToken, null);
 		return tokens;
 	}
 
 	public async revokeToken(token: string): Promise<void> {
-		const body = new URLSearchParams();
-		body.set("token", token);
-		const request = createOAuth2Request(tokenRevocationEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		await sendTokenRevocationRequest(request);
+		await this.client.revokeToken(tokenRevocationEndpoint, token);
 	}
 }
