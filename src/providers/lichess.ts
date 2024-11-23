1be9568a5923dc74bae0ae8a1f7898a32b3cdceb
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-11-23 10:48:55 +0900

    update lichess provider

--- src/providers/lichess.ts
+++ src/providers/lichess.ts
@@ -1,5 +1,4 @@
-import { createS256CodeChallenge } from "../oauth2.js";
-import { createOAuth2Request, sendTokenRequest } from "../request.js";
+import { OAuth2Client, CodeChallengeMethod } from "../client.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
@@ -7,23 +6,19 @@ const authorizationEndpoint = "https://lichess.org/oauth";
 const tokenEndpoint = "https://lichess.org/api/token";
 
 export class Lichess {
-	private clientId: string;
-	private redirectURI: string;
+	private client: OAuth2Client;
 
 	constructor(clientId: string, redirectURI: string) {
-		this.clientId = clientId;
-		this.redirectURI = redirectURI;
+		this.client = new OAuth2Client(clientId, null, redirectURI);
 	}
 	public createAuthorizationURL(state: string, codeVerifier: string, scopes: string[]): URL {
-		const url = new URL(authorizationEndpoint);
-		url.searchParams.set("response_type", "code");
-		url.searchParams.set("client_id", this.clientId);
-		url.searchParams.set("state", state);
-		url.searchParams.set("scope", scopes.join(" "));
-		url.searchParams.set("redirect_uri", this.redirectURI);
-		const codeChallenge = createS256CodeChallenge(codeVerifier);
-		url.searchParams.set("code_challenge_method", "S256");
-		url.searchParams.set("code_challenge", codeChallenge);
+		const url = this.client.createAuthorizationURLWithPKCE(
+			authorizationEndpoint,
+			state,
+			CodeChallengeMethod.S256,
+			codeVerifier,
+			scopes
+		);
 		return url;
 	}
 
@@ -31,13 +26,7 @@ export class Lichess {
 		code: string,
 		codeVerifier: string
 	): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "authorization_code");
-		body.set("code", code);
-		body.set("code_verifier", codeVerifier);
-		body.set("redirect_uri", this.redirectURI);
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.validateAuthorizationCode(tokenEndpoint, code, codeVerifier);
 		return tokens;
 	}
 }
