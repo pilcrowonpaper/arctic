fca82c4839eabb596bce6f91c901fd0767ee8f80
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-11-23 11:19:38 +0900

    update myanimelist

--- src/providers/myanimelist.ts
+++ src/providers/myanimelist.ts
@@ -1,4 +1,4 @@
-import { createOAuth2Request, encodeBasicCredentials, sendTokenRequest } from "../request.js";
+import { OAuth2Client, CodeChallengeMethod } from "../client.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
@@ -6,10 +6,9 @@ const authorizationEndpoint = "https://myanimelist.net/v1/oauth2/authorize";
 const tokenEndpoint = "https://myanimelist.net/v1/oauth2/token";
 
 export class MyAnimeList {
-	private clientId: string;
-	private clientSecret: string;
-	private redirectURI: string | null;
+	private client;
 
+	// v3 TODO: fix
 	constructor(
 		clientId: string,
 		clientSecret: string,
@@ -17,21 +16,17 @@ export class MyAnimeList {
 			redirectURI?: string;
 		}
 	) {
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.redirectURI = options?.redirectURI ?? null;
+		this.client = new OAuth2Client(clientId, clientSecret, options?.redirectURI ?? null);
 	}
 
 	public createAuthorizationURL(state: string, codeVerifier: string): URL {
-		const url = new URL(authorizationEndpoint);
-		url.searchParams.set("response_type", "code");
-		url.searchParams.set("client_id", this.clientId);
-		url.searchParams.set("state", state);
-		if (this.redirectURI !== null) {
-			url.searchParams.set("redirect_uri", this.redirectURI);
-		}
-		url.searchParams.set("code_challenge_method", "plain");
-		url.searchParams.set("code_challenge", codeVerifier);
+		const url = this.client.createAuthorizationURLWithPKCE(
+			authorizationEndpoint,
+			state,
+			CodeChallengeMethod.S256,
+			codeVerifier,
+			[]
+		);
 		return url;
 	}
 
@@ -39,31 +34,12 @@ export class MyAnimeList {
 		code: string,
 		codeVerifier: string
 	): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "authorization_code");
-		body.set("code", code);
-		body.set("code_verifier", codeVerifier);
-		if (this.redirectURI !== null) {
-			body.set("redirect_uri", this.redirectURI);
-		}
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.validateAuthorizationCode(tokenEndpoint, code, codeVerifier);
 		return tokens;
 	}
 
 	public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "refresh_token");
-		body.set("refresh_token", refreshToken);
-		if (this.redirectURI !== null) {
-			body.set("redirect_uri", this.redirectURI);
-		}
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-		request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.refreshAccessToken(tokenEndpoint, refreshToken, []);
 		return tokens;
 	}
 }
