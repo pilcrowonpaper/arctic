06f37ae1c7d818540953186e8bfbea3c5b873371
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-11-23 11:15:17 +0900

    update figma

--- src/providers/figma.ts
+++ src/providers/figma.ts
@@ -1,4 +1,4 @@
-import { createOAuth2Request, sendTokenRequest } from "../request.js";
+import { OAuth2Client } from "../client.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
@@ -6,46 +6,25 @@ const authorizationEndpoint = "https://www.figma.com/oauth";
 const tokenEndpoint = "https://www.figma.com/api/oauth/token";
 
 export class Figma {
-	private clientId: string;
-	private clientSecret: string;
-	private redirectURI: string;
+	private client: OAuth2Client;
 
 	constructor(clientId: string, clientSecret: string, redirectURI: string) {
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.redirectURI = redirectURI;
+		this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
 	}
 
 	public createAuthorizationURL(state: string, scopes: string[]): URL {
-		const url = new URL(authorizationEndpoint);
-		url.searchParams.set("response_type", "code");
-		url.searchParams.set("client_id", this.clientId);
-		url.searchParams.set("state", state);
-		url.searchParams.set("scope", scopes.join(" "));
-		url.searchParams.set("redirect_uri", this.redirectURI);
+		const url = this.client.createAuthorizationURL(authorizationEndpoint, state, scopes);
 		return url;
 	}
 
 	public async validateAuthorizationCode(code: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "authorization_code");
-		body.set("code", code);
-		body.set("redirect_uri", this.redirectURI);
-		body.set("client_id", this.clientId);
-		body.set("client_secret", this.clientSecret);
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.validateAuthorizationCode(tokenEndpoint, code, null);
 		return tokens;
 	}
 
+	// TODO: Remove
 	public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
-		const body = new URLSearchParams();
-		body.set("grant_type", "refresh_token");
-		body.set("refresh_token", refreshToken);
-		body.set("client_id", this.clientId);
-		body.set("client_secret", this.clientSecret);
-		const request = createOAuth2Request(tokenEndpoint, body);
-		const tokens = await sendTokenRequest(request);
+		const tokens = await this.client.refreshAccessToken(tokenEndpoint, refreshToken, []);
 		return tokens;
 	}
 }
