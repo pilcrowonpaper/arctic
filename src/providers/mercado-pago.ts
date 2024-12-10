6cd8deb655e1a618c28a6e7957dfeb601651f55e
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-12-10 12:49:36 -0300

    feat(providers): add mercado pago

---
+++ src/providers/mercado-pago.ts
@@ -0,0 +1,62 @@
+import { CodeChallengeMethod, OAuth2Client } from "../client.js";
+import type { OAuth2Tokens } from "../oauth2.js";
+import { createOAuth2Request, sendTokenRequest } from "../request.js";
+
+const authorizationEndpoint = "https://auth.mercadopago.com.ar/authorization";
+const tokenEndpoint = "https://api.mercadopago.com/oauth/token";
+
+export class MercadoPago {
+	public clientId: string;
+
+	private client: OAuth2Client;
+	private clientSecret: string;
+	private redirectURI: string;
+
+	constructor(clientId: string, clientSecret: string, redirectURI: string) {
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.redirectURI = redirectURI;
+		this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
+	}
+
+	public createAuthorizationURL(state: string, codeVerifier: string, scopes: string[]): URL {
+		const url = this.client.createAuthorizationURLWithPKCE(
+			authorizationEndpoint,
+			state,
+			CodeChallengeMethod.S256,
+			codeVerifier,
+			scopes
+		);
+		return url;
+	}
+
+	public async validateAuthorizationCode(
+		code: string,
+		codeVerifier: string
+	): Promise<OAuth2Tokens> {
+		const body = new URLSearchParams();
+		body.set("grant_type", "authorization_code");
+		body.set("code", code);
+		body.set("redirect_uri", this.redirectURI);
+		body.set("code_verifier", codeVerifier);
+		body.set("client_id", this.clientId);
+		body.set("client_secret", this.clientSecret);
+		const request = createOAuth2Request(tokenEndpoint, body);
+		const tokens = await sendTokenRequest(request);
+		return tokens;
+	}
+
+	public async refreshAccessToken(
+		tokenEndpoint: string,
+		refreshToken: string
+	): Promise<OAuth2Tokens> {
+		const body = new URLSearchParams();
+		body.set("grant_type", "refresh_token");
+		body.set("refresh_token", refreshToken);
+		body.set("client_id", this.clientId);
+		body.set("client_secret", this.clientSecret);
+		const request = createOAuth2Request(tokenEndpoint, body);
+		const tokens = await sendTokenRequest(request);
+		return tokens;
+	}
+}
