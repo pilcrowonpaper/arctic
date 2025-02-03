67f23b5e26c3738182f60f027e5ac2a1348ac5c6
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2025-02-03 22:36:39 +0100

    fix: refactored gitea code to cleaner code

--- src/providers/gitea.ts
+++ src/providers/gitea.ts
@@ -1,102 +1,32 @@
-import {
-	ArcticFetchError,
-	createOAuth2Request,
-	createOAuth2RequestError,
-	encodeBasicCredentials,
-	UnexpectedErrorResponseBodyError,
-	UnexpectedResponseError
-} from "../request.js";
+
 import { OAuth2Tokens } from "../oauth2.js";
+import { OAuth2Client } from "../client.js";
+import { joinURIAndPath } from "../request.js";
 
 export class Gitea {
     private authorizationEndpoint: string;
     private tokenEndpoint: string;
 
-    private clientId: string;
-	private clientSecret: string;
-	private redirectURI: string | null;
+    private client: OAuth2Client;
 
-    constructor(domain: string, clientId: string, clientSecret: string, redirectURI: string | null) {
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.redirectURI = redirectURI;
-        this.authorizationEndpoint = `https://${domain}/login/oauth/authorize`;
-        this.tokenEndpoint = `https://${domain}/login/oauth/access_token`;
-	}
+    constructor(baseURL: string, clientId: string, clientSecret: string | null, redirectURI: string) {
+        this.authorizationEndpoint = joinURIAndPath(baseURL, "/login/oauth/authorize");
+        this.tokenEndpoint = joinURIAndPath(baseURL, "/login/oauth/access_token");
+        this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
+    }
 
     public createAuthorizationURL(state: string, scopes: string[]): URL {
-            const url = new URL(this.authorizationEndpoint);
-            url.searchParams.set("response_type", "code");
-            url.searchParams.set("client_id", this.clientId);
-            url.searchParams.set("state", state);
-            if (scopes.length > 0) {
-                url.searchParams.set("scope", scopes.join(" "));
-            }
-            if (this.redirectURI !== null) {
-                url.searchParams.set("redirect_uri", this.redirectURI);
-            }
-            return url;
-        }
-    
-        public async validateAuthorizationCode(code: string): Promise<OAuth2Tokens> {
-            const body = new URLSearchParams();
-            body.set("grant_type", "authorization_code");
-            body.set("code", code);
-            if (this.redirectURI !== null) {
-                body.set("redirect_uri", this.redirectURI);
-            }
-            const request = createOAuth2Request(this.tokenEndpoint, body);
-            const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-            request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-            const tokens = await sendTokenRequest(request);
-            return tokens;
-        }
-    
-        public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
-            const body = new URLSearchParams();
-            body.set("grant_type", "refresh_token");
-            body.set("refresh_token", refreshToken);
-            const request = createOAuth2Request(this.tokenEndpoint, body);
-            const encodedCredentials = encodeBasicCredentials(this.clientId, this.clientSecret);
-            request.headers.set("Authorization", `Basic ${encodedCredentials}`);
-            const tokens = await sendTokenRequest(request);
-            return tokens;
-        }
+        const url = this.client.createAuthorizationURL(this.authorizationEndpoint, state, scopes);
+        return url;
     }
-    
-    async function sendTokenRequest(request: Request): Promise<OAuth2Tokens> {
-        let response: Response;
-        try {
-            response = await fetch(request);
-        } catch (e) {
-            throw new ArcticFetchError(e);
-        }
-    
-        if (response.status !== 200) {
-            if (response.body !== null) {
-                await response.body.cancel();
-            }
-            throw new UnexpectedResponseError(response.status);
-        }
-    
-        let data: unknown;
-        try {
-            data = await response.json();
-        } catch {
-            throw new UnexpectedResponseError(response.status);
-        }
-        if (typeof data !== "object" || data === null) {
-            throw new UnexpectedErrorResponseBodyError(response.status, data);
-        }
-        if ("error" in data && typeof data.error === "string") {
-            let error: Error;
-            try {
-                error = createOAuth2RequestError(data);
-            } catch {
-                throw new UnexpectedErrorResponseBodyError(response.status, data);
-            }
-            throw error;
-        }
-        const tokens = new OAuth2Tokens(data);
+
+    public async validateAuthorizationCode(code: string): Promise<OAuth2Tokens> {
+        const tokens = await this.client.validateAuthorizationCode(this.tokenEndpoint, code, null);
         return tokens;
+    }
+
+    public async refreshAccessToken(refreshToken: string): Promise<OAuth2Tokens> {
+        const tokens = await this.client.refreshAccessToken(this.tokenEndpoint, refreshToken, []);
+        return tokens;
+    }
 }
